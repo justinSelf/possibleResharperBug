87cc633e6610af28959e2591fe60fd4ad73cb5b5
Justin Self <justinself@outlook.com>
2019-05-05 12:01:45 -0500

    Adds a small comment to explain what I'm seeing

--- ResharperFinallyBug/Program.cs
+++ ResharperFinallyBug/Program.cs
@@ -25,11 +25,14 @@ namespace ResharperFinallyBug
             }
             catch
             {
+                //ReSharper is telling me that success is not used and implying it is safe to delete.
+                //however, if I delete it, then there is a bug in the finally block.
                 success = false;
                 throw;
             }
             finally
             {
+                //This will print false
                 Console.WriteLine($"Success: {success}");
                 Console.ReadKey();
             }
