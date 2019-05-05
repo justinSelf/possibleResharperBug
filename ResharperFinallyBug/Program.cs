8abe04d5e0f60f50f1d37d7d44176fa45a98fc69
Justin Self <justinself@outlook.com>
2019-05-05 11:59:05 -0500

    Adds a small POC for the potential bug in resharper

---
+++ ResharperFinallyBug/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ResharperFinallyBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                DoTest();
+            }
+            catch
+            {
+                //ignoring
+            }
+        }
+
+        static void DoTest()
+        {
+            var success = true;
+            try
+            {
+                throw new Exception();
+            }
+            catch
+            {
+                success = false;
+                throw;
+            }
+            finally
+            {
+                Console.WriteLine($"Success: {success}");
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
